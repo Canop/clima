a89296d6cdd81ef044a4a9a57f28f5704987f9c7
Canop <cano.petrole@gmail.com>
2019-12-26 21:25:11 +0100

    use a new version of termimad (allowing code fences)

--- src/viewer.rs
+++ src/viewer.rs
@@ -1,44 +1,54 @@
-use crossterm::{
-    cursor,
-    input::{self, InputEvent, KeyEvent},
-    queue,
-    screen::{EnterAlternateScreen, LeaveAlternateScreen, RawScreen},
-    style::Color::*,
+use {
+    crossterm::{
+        cursor,
+        event::{
+            self,
+            Event,
+            KeyEvent,
+            KeyCode::*,
+        },
+        terminal::{self, EnterAlternateScreen, LeaveAlternateScreen},
+        style::Color::*,
+        QueueableCommand,
+    },
+    std::{
+        fs,
+        io::{self, Write},
+        path::Path,
+    },
+    termimad::{
+        Alignment, Area, MadSkin, MadView, Result,
+    },
 };
-use std::{
-    fs,
-    io::{self, Write},
-    path::Path,
-};
-use termimad::{Alignment, Area, MadSkin, MadView, Result};
 
 fn run_scrollable(w: &mut io::Stderr, area: Area, skin: MadSkin, markdown: &str) -> Result<()> {
+    terminal::enable_raw_mode()?;
     let mut view = MadView::from(markdown.to_owned(), area, skin);
-    let mut events = input::input().read_sync();
     loop {
         view.write_on(w)?;
         w.flush()?;
-        if let Some(InputEvent::Keyboard(key)) = events.next() {
-            match key {
-                KeyEvent::Up => view.try_scroll_lines(-1),
-                KeyEvent::Down => view.try_scroll_lines(1),
-                KeyEvent::PageUp => view.try_scroll_pages(-1),
-                KeyEvent::PageDown => view.try_scroll_pages(1),
+        if let Ok(Event::Key(KeyEvent{code, ..})) = event::read() {
+            match code {
+                Up => view.try_scroll_lines(-1),
+                Down => view.try_scroll_lines(1),
+                PageUp => view.try_scroll_pages(-1),
+                PageDown => view.try_scroll_pages(1),
                 _ => break,
             }
         }
     }
+    terminal::disable_raw_mode()?;
     Ok(())
 }
 
 fn show_path(w: &mut io::Stderr, y: u16, skin: &MadSkin, target: &Path) -> Result<()> {
-    queue!(w, cursor::MoveTo(0, y))?;
+    w.queue(cursor::MoveTo(0, y))?;
     let path = target.to_string_lossy();
     mad_write_inline!(w, skin, "**Clima >** *$0*", &path)
 }
 
 fn show_help(w: &mut io::Stderr, y: u16, skin: &MadSkin) -> Result<()> {
-    queue!(w, cursor::MoveTo(0, y))?;
+    w.queue(cursor::MoveTo(0, y))?;
     mad_write_inline!(
         w,
         skin,
@@ -65,16 +75,15 @@ pub fn run(launch_args: crate::cli::AppLaunchArgs) -> Result<()> {
         skin.print_text(&markdown);
     } else {
         let mut w = std::io::stderr();
-        queue!(w, EnterAlternateScreen)?;
-        queue!(w, cursor::Hide)?; // hiding the cursor
-        let _raw_screen = RawScreen::into_raw_mode()?;
+        w.queue(EnterAlternateScreen)?;
+        w.queue(cursor::Hide)?; // hiding the cursor
         let mut main_area = Area::full_screen();
         main_area.pad(0, 1);
         show_path(&mut w, 0, &skin, &target)?;
         show_help(&mut w, main_area.top + main_area.height + 1, &skin)?;
         run_scrollable(&mut w, main_area, skin, &markdown)?;
-        queue!(w, cursor::Show)?;
-        queue!(w, LeaveAlternateScreen)?;
+        w.queue(cursor::Show)?;
+        w.queue(LeaveAlternateScreen)?;
         w.flush()?;
     }
     Ok(())
