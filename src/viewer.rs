8505dd06e99ca67125edec3d6d01fafcb8e896f5
Canop <cano.petrole@gmail.com>
2020-02-25 10:58:51 +0100

    more compatible arrow characters

--- src/viewer.rs
+++ src/viewer.rs
@@ -3,6 +3,8 @@ use {
         cursor,
         event::{
             self,
+            DisableMouseCapture,
+            EnableMouseCapture,
             Event,
             KeyEvent,
             KeyCode::*,
@@ -27,7 +29,9 @@ fn run_scrollable(w: &mut io::Stderr, area: Area, skin: MadSkin, markdown: &str)
     loop {
         view.write_on(w)?;
         w.flush()?;
-        if let Ok(Event::Key(KeyEvent{code, ..})) = event::read() {
+        let event = event::read();
+        debug!("event: {:?}", event);
+        if let Ok(Event::Key(KeyEvent{code, ..})) = event {
             match code {
                 Up => view.try_scroll_lines(-1),
                 Down => view.try_scroll_lines(1),
@@ -52,7 +56,7 @@ fn show_help(w: &mut io::Stderr, y: u16, skin: &MadSkin) -> Result<()> {
     mad_write_inline!(
         w,
         skin,
-        "Use the **🡑** and **🡓** arrow keys to scroll, any other key to quit"
+        "Use the **↓** and **↑** arrow keys to scroll, any other key to quit"
     )
 }
 
@@ -81,7 +85,9 @@ pub fn run(launch_args: crate::cli::AppLaunchArgs) -> Result<()> {
         main_area.pad(0, 1);
         show_path(&mut w, 0, &skin, &target)?;
         show_help(&mut w, main_area.top + main_area.height + 1, &skin)?;
+        w.queue(EnableMouseCapture)?;
         run_scrollable(&mut w, main_area, skin, &markdown)?;
+        w.queue(DisableMouseCapture)?;
         w.queue(cursor::Show)?;
         w.queue(LeaveAlternateScreen)?;
         w.flush()?;
