bac0d5dff6ec884b9f6aba212469db37f5212294
Canop <cano.petrole@gmail.com>
2021-08-09 21:58:19 +0200

    version 0.0.9

--- src/viewer.rs
+++ src/viewer.rs
@@ -62,7 +62,7 @@ fn show_help(w: &mut io::Stderr, y: u16, skin: &MadSkin) -> Result<()> {
 fn make_skin() -> MadSkin {
     match terminal_light::luma() {
         Ok(luma) if luma > 0.6 => MadSkin::default_light(),
-        Ok(luma) => MadSkin::default_dark(),
+        Ok(_) => MadSkin::default_dark(),
         Err(_) => MadSkin::default(), // this skin works in both light and dark
     }
 }
