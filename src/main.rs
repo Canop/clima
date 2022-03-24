c82869c3f54a1873ed81a4f95dea849fdb405a38
Canop <cano.petrole@gmail.com>
2022-03-24 20:25:43 +0100

    version 0.1

--- src/main.rs
+++ src/main.rs
@@ -1,56 +1,20 @@
 #[macro_use]
-extern crate log;
+extern crate cli_log;
 
 mod cli;
 mod errors;
 mod open;
 mod viewer;
 
-use {
-    crate::{
-        cli::get_cli_args,
-        errors::ProgramError,
-    },
-    log::LevelFilter,
-    std::{
-        env,
-        fs::File,
-        str::FromStr,
-    },
+use crate::{
+    cli::get_cli_args,
+    errors::ProgramError,
 };
-
-
-/// configure the application log according to env variable.
-///
-/// There's no log unless the CLIMA_LOG environment variable is set to
-///  a valid log level (trace, debug, info, warn, error, off)
-/// Example:
-///      CLIMA_LOG=info clima
-/// As clima is a terminal application, we only log to a file (dev.log)
-fn configure_log() {
-    let level = env::var("CLIMA_LOG").unwrap_or_else(|_| "off".to_string());
-    if level == "off" {
-        return;
-    }
-    if let Ok(level) = LevelFilter::from_str(&level) {
-        simplelog::WriteLogger::init(
-            level,
-            simplelog::Config::default(),
-            File::create("dev.log").expect("Log file can't be created"),
-        )
-        .expect("log initialization failed");
-        info!(
-            "Starting Clima v{} with log level {}",
-            env!("CARGO_PKG_VERSION"),
-            level
-        );
-    }
-}
 
 /// run the application, and maybe return a launchable
 /// which must be run after clima
 fn run() -> Result<(), ProgramError> {
-    configure_log();
+    init_cli_log!();
     let cli_args = get_cli_args();
     let target = match cli_args.value_of("target") {
         Some(path) => path.to_string(),
